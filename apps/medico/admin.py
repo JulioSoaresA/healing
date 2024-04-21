f79f9049a8297affe3332ec1a0753c7a99e4a232
Júlio Soares <juliocsoaresa1@gmail.com>
2024-04-20 21:40:52 -0300

    Feat: Cadastro medico

--- apps/medico/admin.py
+++ apps/medico/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Especialidades, DadosMedico
 
-# Register your models here.
+class EspecialidadesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'especialidade')
+    list_display_links = ('id', 'especialidade',)
+    list_filter = ('especialidade',)
+    filter_horizontal = ()
+    fieldsets = ()
+
+
+class DadosMedicoAdmin(admin.ModelAdmin):
+    list_display = ('id','crm', 'nome', 'especialidade',)
+    list_display_links = ('id', 'crm', 'nome',)
+    list_filter = ('crm', 'nome', 'especialidade',)
+    filter_horizontal = ()
+    fieldsets = ()
+
+admin.site.register(Especialidades, EspecialidadesAdmin)
+admin.site.register(DadosMedico, DadosMedicoAdmin)
