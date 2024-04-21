e732abf3cadf83ad45512be403f0987c094caa1a
Júlio Soares <juliocsoaresa1@gmail.com>
2024-04-20 23:50:20 -0300

    Feat: Escolher consultas

--- apps/paciente/views.py
+++ apps/paciente/views.py
@@ -17,3 +17,10 @@ def home(request):
             medicos = medicos.filter(especialidade_id__in=especialidades_filtrar)
 
         return render(request, 'pacientes/home.html', locals())
+
+
+def escolher_horario(request, id_dados_medicos):
+    if request.method == "GET":
+        medico = DadosMedico.objects.get(id=id_dados_medicos)
+        datas_abertas = DatasAbertas.objects.filter(user=medico.user, data__gte=datetime.now(), agendado=False)
+        return render(request, 'pacientes/escolher_horario.html', locals())
